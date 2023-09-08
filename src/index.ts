a1c833e9f5d57d050ba5da353fb7e3f3a42100d8
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-09-08 16:04:46 -0700

    add some comment for improvement!

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import path from 'path';
 
 const app = express();
 
+// name of type can be UserType
 type DataType = {
-    email: string;
-    password: string;
-}
+  email: string;
+  password: string;
+};
 
 const users: DataType[] = [];
 
@@ -18,39 +19,43 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.render('home', { title: 'HOME' });
+  res.render('home', { title: 'HOME' });
 });
 
 app.get('/login', (req, res) => {
-    res.render('login', { title: 'LOGIN FORM' });
+  res.render('login', { title: 'LOGIN FORM' });
 });
 
 app.get('/register', (req, res) => {
-    res.render('register', { title: 'REGISTER FORM' });
+  res.render('register', { title: 'REGISTER FORM' });
 });
 
 //register route
 app.post('/register', (req, res) => {
-    const { email, password } = req.body;
-    const newUser = { email, password };
-    users.push(newUser);
-    res.redirect('/login');
+  const { email, password } = req.body;
+  // validate if user is existing already.
+  // should not allow user to register if existing
+  const newUser = { email, password };
+  users.push(newUser);
+  res.redirect('/login');
 });
 
 //login route
 app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    const user = users.find((u) => u.email === email && u.password === password);
-    if (user) {
-        res.redirect('/');
-    } else {
-        res.redirect('/login');
-    }
+  const { email, password } = req.  body;
+  // validate credential separately
+  // if not validated, then throw some errors
+  const user = users.find((u) => u.email === email && u.password === password);
+  if (user) {
+    res.redirect('/');
+  } else {
+    res.redirect('/login');
+  }
 });
-
+// try to organize your variables
 const PORT = 3000;
 const HOST = 'localhost';
 
 app.listen(PORT, HOST, () => {
-    console.log(`Server is running on http://${HOST}:${PORT}`);
+  console.log(`Server is running on http://${HOST}:${PORT}`);
 });
